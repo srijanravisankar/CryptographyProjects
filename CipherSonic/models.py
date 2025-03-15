7f4274581af5771b433e9cc056bb4270b80b5a1a
Srijan Ravisankar <srijanvr@gmail.com>
2025-03-15 12:00:37 -0400

    added database functionality

---
+++ CipherSonic/models.py
@@ -0,0 +1,12 @@
+from flask_sqlalchemy import SQLAlchemy
+from app import app
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.username}')"
